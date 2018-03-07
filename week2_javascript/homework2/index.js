0824bc53fb9f2a84c2b4e77c2793ce0b170240c8
lucianlee <lucianlee1311@gmail.com>
2018-03-07 17:14:21 +0800

    update homework2

--- week2_javascript/homework2/index.js
+++ week2_javascript/homework2/index.js
@@ -3,25 +3,51 @@
  * 1.一個陣列中有許多個字串，寫一個function找出這些字串最長的共同字首
  * 範例： ['abcd','abccc','abdec'] --> 共同字首為 'ab' 。
  */
-const myArray = ['abcd', 'abccc', 'abdec'];
-let result = myArray[0];
-for (let i = 1; i < myArray.length; i += 1) {
-  const str = myArray[i];
-  let sameLength = 0;
-  for (let j = 0; j < result.length; j += 1) {
-    if (result[j] !== str.charAt(j)) {
-      sameLength = j;
-      break;
-    }
-  }
-  result = result.substr(0, sameLength);
-}
-console.log(result);
+/** v.2 */
+// const myArray = ['abcd', 'abccc', 'abdec'];
+// let result = myArray[0];
+// myArray.forEach((entry, i) => {
+//   if (i === 0) {
+//     return;
+//   }
+//   const str = entry;
+//   let sameLength = 0;
+//   Array.from(result).forEach((item, j) => {
+//     if (item === str.charAt(j)) {
+//       sameLength = j + 1;
+//     }
+//   });
+//   result = result.substr(0, sameLength);
+// });
+// console.log(result);
+
+/** v.1 */
+// const myArray = ['abcd', 'abccc', 'abdec'];
+// let result = myArray[0];
+// for (let i = 1; i < myArray.length; i += 1) {
+//   const str = myArray[i];
+//   let sameLength = 0;
+//   for (let j = 0; j < result.length; j += 1) {
+//     if (result[j] === str.charAt(j)) {
+//       sameLength = j + 1;
+//     }
+//   }
+//   result = result.substr(0, sameLength);
+// }
+// console.log(result);
+
 
 /**
  * 2.將一個字串反轉後回傳
  * 範例： s= "hello", return "olleh"
  */
+/** v.2 */
+// const str = 'hello';
+// let strAry = Array.from(str);
+// strAry = strAry.reverse().join('');
+// console.log(strAry);
+
+/** v.1 */
 // const str = 'hello';
 // let newStr = '';
 // for (let i = str.length - 1; i >= 0; i -= 1) {
@@ -35,6 +61,28 @@ console.log(result);
  * s = "anagram", t = "nagaram" 回傳true
  * s = "rat", t = "car" 回傳false
  */
+/** v.2 */
+// const s = 'anagram';//'rat';
+// const t = 'nagram';//'car';
+// let result = true;
+// let location = 0;
+// let breakFlag = false;
+
+// Array.from(t).forEach((tElement) => {
+//   if (breakFlag) {
+//     return;
+//   }
+//   location = s.indexOf(tElement, location);
+//   if (location < 0) {
+//     result = false;
+//     breakFlag = true;
+//   } else {
+//     location += 1;
+//   }
+// });
+// console.log(result);
+
+/** v.1 */
 // const s = 'anagram';//'rat';
 // const t = 'nagram';//'car';
 // let result = false;
@@ -51,6 +99,26 @@ console.log(result);
  * Given s = "leetcode", return "leotcede".
  * 注意： y不算在母音字母中。
  */
+/** v.2 */
+// const s = 'hello';
+// const vowelStr = 'aeiou';
+// const vowelStrArray = [];
+// let newStr = '';
+
+// Array.from(s).forEach((element) => {
+//   if (vowelStr.indexOf(element) > -1) {
+//     vowelStrArray.push(element);
+//     newStr += '@';
+//   } else {
+//     newStr += element;
+//   }
+// });
+// vowelStrArray.reverse().forEach((element) => {
+//   newStr = newStr.replace('@', element);
+// });
+// console.log(newStr);
+
+/** v.1 */
 // const s = 'hello';
 // const vowel = ['a', 'e', 'i', 'o', 'u'];
 // const vowelObjArray = [];
@@ -119,6 +187,20 @@ console.log(result);
  * 範例：
  * 輸入：65 輸出：'01000001'
  */
+/** v.2 */
+// const convertBinary = (decimal) => {
+//   const binary = decimal.toString(2);
+//   const binaryArray = Array.from(binary);
+//   const zeroLength = 8 - binaryArray.length;
+
+//   for (let i = 0; i < zeroLength; i += 1) {
+//     binaryArray.reverse().push('0');
+//   }
+//   return binaryArray.reverse().join('');
+// };
+// console.log(convertBinary(65));
+
+/** v.1 */
 // const process = (decimal, modArray) => {
 //   let quotient = decimal / 2;
 //   const mod = decimal % 2;
@@ -133,7 +215,6 @@ console.log(result);
 //   let result = '';
 //   let modArray = [];
 //   modArray = process(decimal, modArray);
-
 //   const zeroLength = 8 - modArray.length;
 //   for (let i = 0; i < zeroLength; i += 1) {
 //     result += '0';
@@ -150,6 +231,7 @@ console.log(result);
  * 範例：
  * num = 38，則 3+8 = 11，1+1 = 2, 2是個位數，回傳2。
  */
+/** v.1 */
 // let num = 38;
 // let strNumArray = [];
 // let sum = 0;
@@ -168,6 +250,17 @@ console.log(result);
  * 範例：
  * x = 123 , return 321 x = -123 , return -321
  */
+/** v.2 */
+// const num = 123;
+// const strNum = Math.abs(num).toString();
+// const newStr = Array.from(strNum).reverse().join('');
+// let result = parseInt(newStr, 10);
+// if (num < 0) {
+//   result = -result;
+// }
+// console.log(result);
+
+/** v.1 */
 // const num = 123;
 // const strNum = Math.abs(num).toString();
 // let newStr = '';
