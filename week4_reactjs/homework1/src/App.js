d3733ebc7fcada06fab059c8c5944be3a467d419
lucianlee <lucianlee1311@gmail.com>
2018-04-03 11:44:00 +0800

    add reactjs homework1

---
+++ week4_reactjs/homework1/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Profile from './Profile.js';
+
+class App extends Component {
+  state = {
+    current: 0,
+    list: []
+  }
+  componentDidMount() {
+    const list = Array.from(Array(10), (val,index) => { 
+      return {id: index+1, current: index+1}
+    });
+    this.setState({list});
+  }
+  myButtonEvent = () => {
+    const current = this.state.current += 1;
+    this.setState({current});
+  }
+  onClickProfile = (data) => {
+    const dataIndex = this.state.list.findIndex((item) => {
+      return item.id === data.id;
+    });
+    const list = this.state.list.slice();
+    list[dataIndex].current += 1;
+    this.setState({list});
+  }
+  render() {
+    return (
+      <div className="App">
+        <div>{this.state.list.map((item) =><Profile key={item.id} number={item.current} data={item} onClickProfile={this.onClickProfile}/>)}</div>
+        <button onClick={this.myButtonEvent}>app click</button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ week4_reactjs/homework1/src/Profile.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Profile extends React.Component {
+  static propTypes = {
+    number: PropTypes.number.isRequired,
+    data: PropTypes.shape({id: PropTypes.number, current: PropTypes.number}),
+    onClickProfile: PropTypes.func
+  }
+  clickProfile = () => {
+    this.props.onClickProfile(this.props.data);
+  }
+  render() {
+    return (
+      <div>
+        <span>hi this is profile {this.props.number}.</span>
+        <button onClick={this.clickProfile}>profile click</button>
+      </div>
+    );
+  }
+}
+
+export default Profile;
